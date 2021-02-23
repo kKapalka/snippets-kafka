47649d8354d1efc76a267882a017f3dfc5e5691d
Kamil Kapalka <kamil.kapalka@comarch.pl>
2021-02-23 08:06:18 +0100

    working processor

---
+++ kafka-processor/src/main/java/org/example/snippets/kafka/processor/KafkaProcessorConfig.java
@@ -0,0 +1,21 @@
+package org.example.snippets.kafka.processor;
+
+import java.util.function.Function;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProcessorConfig {
+    @Bean
+    public Function<KStream<String, String>, KStream<String, String>> process(){
+        return input -> input.map((this::reverseValue)).filter(((key, value) -> value != null));
+    }
+
+
+    private KeyValue<String, String> reverseValue(String key, String value) {
+        System.out.println("Received: key: "+key+", value: "+value);
+        return new KeyValue<>(key, new StringBuilder().append(value).reverse().toString());
+    }
+}
