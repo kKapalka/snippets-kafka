662e1120b250c7164b1cda256e9477bf67507d85
Kamil Kapalka <kamil.kapalka@comarch.pl>
2021-02-23 06:15:17 +0100

    first commit

---
+++ src/main/java/com/example/snippets/SnippetsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.snippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetsKafkaApplication.class, args);
+	}
+
+}
