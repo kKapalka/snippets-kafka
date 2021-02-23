7330454d11bc4df0cf03f9d58879df8a2b25e927
Kamil Kapalka <kamil.kapalka@comarch.pl>
2021-02-23 08:38:56 +0100

    working consumer

--- kafka-consumer/src/main/java/org/example/snippets/kafka/consumer/KafkaConsumerConfig.java
+++ kafka-consumer/src/main/java/org/example/snippets/kafka/consumer/KafkaConsumerConfig.java
@@ -3,11 +3,13 @@ package org.example.snippets.kafka.consumer;
 import java.util.function.Consumer;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class KafkaConsumerConfig {
 
     @Bean
-    public Consumer<KStream<String, String>> consume(){
+    public Consumer<KStream<String, String>> consumer(){
         return input -> input.foreach((this::display));
     }
 
